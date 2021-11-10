024c1e3d98c7749e3d7d36f70dc592b7ed17b4ae
Magnus Hvidtfeldt <93657779+mch-sg@users.noreply.github.com>
2021-11-10 14:16:01 +0100

    Initial-upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+//      LOSE FUNCTION       //
+
+document.getElementById("mine").onclick = function() {
+    if(this.checked) { document.getElementById('lose').style.visibility = 'visible'; } 
+    else { document.getElementById('lose').style.visibility = 'hidden'; }
+}
+
+document.getElementById("mine2").onclick = function() {
+    if(this.checked) { document.getElementById('lose').style.visibility = 'visible'; } 
+    else { document.getElementById('lose').style.visibility = 'hidden'; }
+} 
+
+document.getElementById("mine3").onclick = function() {
+    if(this.checked) { document.getElementById('lose').style.visibility = 'visible'; } 
+    else { document.getElementById('lose').style.visibility = 'hidden'; }
+} 
+
+document.getElementById("mine4").onclick = function() {
+    if(this.checked) { document.getElementById('lose').style.visibility = 'visible'; } 
+    else { document.getElementById('lose').style.visibility = 'hidden'; }
+} 
+
+document.getElementById("mine5").onclick = function() {
+    if(this.checked) { document.getElementById('lose').style.visibility = 'visible'; } 
+    else {  document.getElementById('lose').style.visibility = 'hidden'; }
+} 
+
+
+
+//      WIN FUNCTION        //
+
+document.getElementById("wm1").onclick = function() {
+    if(this.checked) { document.getElementById('win').style.visibility = 'visible'; } 
+    else {  document.getElementById('win').style.visibility = 'hidden'; }
+} 
+
+document.getElementById("wm2").onclick = function() {
+    if(this.checked) { document.getElementById('win').style.visibility = 'visible'; } 
+    else {  document.getElementById('win').style.visibility = 'hidden'; }
+} 
+
